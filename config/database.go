8d3cad02752eeb909c24e75c37a5743306907ad5
Bred Lobo <bredlobo2@gmail.com>
2026-04-30 15:37:44 +0700

    Update konfigurasi database dinamis untuk deploy

--- config/database.go
+++ config/database.go
@@ -1,27 +1,28 @@
 package config
 
 import (
-	"fmt"
-	"log"
-
+	"os"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-// DB adalah variabel global untuk akses database
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	// Format: user:password@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local
-	// Jika pakai XAMPP standar tanpa password, biarkan root:
-	dsn := "root:@tcp(127.0.0.1:3306)/rupiahcare_db?charset=utf8mb4&parseTime=True&loc=Local"
-	
-	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// 1. Coba ambil URL Database dari setting server (misal dari Render nanti)
+	dsn := os.Getenv("DB_URL") 
 
-	if err != nil {
-		log.Fatal("❌ Gagal terhubung ke database:", err)
+	// 2. Kalau kosong (berarti kamu lagi nge-run di laptop sendiri)
+	if dsn == "" {
+		// Gunakan database XAMPP lokalmu
+		dsn = "root:@tcp(127.0.0.1:3306)/rupiahcare?charset=utf8mb4&parseTime=True&loc=Local"
 	}
 
+	// 3. Lakukan koneksi
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Gagal koneksi ke database!")
+	}
+	
 	DB = database
-	fmt.Println("✅ Berhasil terhubung ke database rupiahcare_db!")
 }
